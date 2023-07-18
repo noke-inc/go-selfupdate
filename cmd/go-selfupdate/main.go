4da648c754c45ae47a8564d0fdee4e6b2aff476a
preston.delano@noke.com <preston.delano@noke.com>
2023-07-18 10:42:53 -0600

    bugfix

--- cmd/go-selfupdate/main.go
+++ cmd/go-selfupdate/main.go
@@ -69,8 +69,10 @@ func createUpdate(path string, platform string) {
 
 	c = append(c, current{Version: version, Path: path, Sha256: generateSha256(path)})
 	for _, file := range files {
-		filePath := filepath.Join(genDir, file.Name())
-		c = append(c, current{Version: file.Name(), Path: filePath, Sha256: generateSha256(filePath)})
+		if file.IsDir() {
+			filePath := filepath.Join(genDir, file.Name())
+			c = append(c, current{Version: file.Name(), Path: filePath, Sha256: generateSha256(filePath)})
+		}
 	}
 
 	b, err := json.MarshalIndent(c, "", "    ")
