89c0c45006fed314ab4300f873fb916244acf39b
preston.delano@noke.com <preston.delano@noke.com>
2023-07-18 18:50:52 -0600

    bugfix

--- cmd/go-selfupdate/main.go
+++ cmd/go-selfupdate/main.go
@@ -74,7 +74,7 @@ func createUpdate(path string, platform string) {
 	}
 
 	os.MkdirAll(filepath.Join(genDir, version), 0755)
-	fmt.Println("gendir: " + genDir + " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	fmt.Println("gendir: " + genDir + " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! path: " + path)
 	err = ioutil.WriteFile(filepath.Join(genDir, version, platform+".json"), b, 0755)
 	if err != nil {
 		panic(err)
